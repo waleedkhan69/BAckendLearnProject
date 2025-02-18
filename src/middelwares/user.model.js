80b6cd9001dec4842e25ca60f71e2b14d899c3f3
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-02-19 02:45:16 +0500

    multer

--- src/middelwares/user.model.js
+++ src/middelwares/user.model.js
@@ -4,54 +4,71 @@ import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
- username: {
-  type: String,
-  unique: true,
-  required: true,
-  trim: true,
-  lowercase: true,
-  index: true
- },
- email: {
-  type: String,
-  unique: true,
-  required: true,
-  trim: true,
-  lowercase: true,
-
- },
- fullNmae: {
-  type: String,
-  required: true,
-  trim: true,
-  index: true
- },
- avater: {
-  type: String,
-  required: true,
- },
- coverImage: {
-  type: String, //cloudinary url
- },
- watchHistory: [
-  {
-   type: Schema.Types.ObjectId,
-   ref: "vedio"
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    index: true
   },
- ],
- password: {
-  type: String,
-  required: [true, "password is required"]
- },
- refirshToken: {
-  type: String
- },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+
+  },
+  fullNmae: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true
+  },
+  avater: {
+    type: String,
+    required: true,
+  },
+  coverImage: {
+    type: String, //cloudinary url
+  },
+  watchHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "vedio"
+    },
+  ],
+  password: {
+    type: String,
+    required: [true, "password is required"]
+  },
+  refirshToken: {
+    type: String
+  },
 
 }, { timestamps: true })
 
 
 userSchema.pre("save", async function (next) {
- if (!this)
-  this.password = bcrypt.hash(this.password, 10)
+  if (!this)
+    this.password = bcrypt.hash(this.password, 10)
+  next()
 })
+userSchema.methods.ispasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+
+  jwt.sign({
+    _id: this._id,
+    email: "",
+    userName: this.userName,
+    fullNmame: this.fullNmae
+
+
+  })
+}
+userSchema.methods.generateRefreshToken = function () { }
 const User = mongoose.model("User", userSchema)
---
+++ src/utils/cloudanary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+ cloud_Name: process.env.CLOUDINARY_CLOUD_NAME,
+ api_key: process.env.CLOUDINARY_API_KEY,
+ api_secret: process.env.CLOUDINARY_API_SCRIPT
+})
+
+const UploadOnCloudinary = async (filePath) => {
+
+ try {
+  if (!filePath) return null
+  const response = await cloudinary.uploader.upload(filePath, {
+   resource_type: "auto"
+  })
+  console.log("file upload successfuly",
+   response.url
+  );
+
+
+ } catch (error) {
+  fs.unlinkSync()
+ }
+
+}
+
+
+
+
+cloudinary.v2.uploader.upload("/home/my_image.jpg", { upload_preset: "my_preset" }, (error, result) => {
+ console.log(result, error);
+});
