827ed5d5c6971a98a75316fad9e3f2f8ef911c93
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-03-06 23:45:08 +0500

    test api

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,7 +56,7 @@ const userSchema = new Schema(
 
 // Hash password before saving
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next(); // Only hash if password is modified
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
